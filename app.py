e9e0384fdb55e900710afeeabcc86d62aa210bc5
CristianJGarcia <garcia.cristian@outlook.com>
2021-02-23 19:36:08 -0600

    route for create show now working with db

--- app.py
+++ app.py
@@ -270,6 +270,7 @@ def create_venue_submission():
         # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
         flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
         db.session.rollback()
+        print(sys.exc_info())
 
     finally:
         db.session.close()
@@ -554,12 +555,25 @@ def create_shows():
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
+    try:
+        form = request.form
+        show = Shows(artist_id=form['artist_id'], venue_id=form['venue_id'], start_time=form['start_time'])
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+        db.session.add(show)
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Show was successfully listed!')
+
+    except:
+        # TODO: on unsuccessful db insert, flash an error instead.
+        # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+        flash('An error occurred. Show could not be listed.')
+        db.session.rollback()
+        print(sys.exc_info())
+
+    finally:
+        db.session.close()
+
     return render_template('pages/home.html')
 
 
